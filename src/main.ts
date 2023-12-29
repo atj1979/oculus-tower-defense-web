95f8b1c9b48034cf70cbb35e124fb5a20c46c22e
atj1979@gmail.com <atj1979@gmail.com>
2023-12-28 16:20:38 -0800

    Vite build

---
+++ src/main.ts
@@ -0,0 +1,6 @@
+import * as THREE from "three";
+// import { VRButton } from 'three/addons/webxr/VRButton.js';
+
+// document.body.appendChild( VRButton.createButton( renderer ) );
+
+console.log(THREE);
